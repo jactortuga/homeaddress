92b6a195e308c44c7918a45c3ab45908fd19e06a
jactortuga <jac@tortugalabs.io>
2017-12-03 12:23:09 +0000

    javascript code commented

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
+
   console.log('Hello Octopus!');
 
+  // Set app variables
   var countyList  = ['Avon', 'Bedfordshire', 'Berkshire', 'Buckinghamshire', 'Cambridgeshire', 'Cheshire', 'Cleveland', 'Cornwall', 'Cumbria', 'Derbyshire', 'Devon', 'Dorset', 'Durham', 'East Sussex', 'Essex', 'Gloucestershire', 'Hampshire', 'Herefordshire', 'Hertfordshire', 'Isle of Wight', 'Kent', 'Lancashire', 'Leicestershire', 'Lincolnshire', 'Greater London', 'Merseyside', 'Middlesex', 'Norfolk', 'Northamptonshire', 'Northumberland', 'North Humberside', 'North Yorkshire', 'Nottinghamshire', 'Oxfordshire', 'Rutland', 'Shropshire', 'Somerset', 'South Humberside', 'South Yorkshire', 'Staffordshire', 'Suffolk', 'Surrey', 'Tyne and Wear', 'Warwickshire', 'West Midlands', 'West Sussex', 'West Yorkshire', 'Wiltshire', 'Worcestershire', 'Clwyd', 'Dyfed', 'Gwent', 'Gwynedd', 'Mid Glamorgan', 'Powys', 'South Glamorgan', 'West Glamorgan', 'Aberdeenshire', 'Angus', 'Argyll', 'Ayrshire', 'Banffshire', 'Berwickshire', 'Bute', 'Caithness', 'Clackmannanshire', 'Dumfriesshire', 'Dunbartonshire', 'East Lothian', 'Fife', 'Inverness-shire', 'Kincardineshire', 'Kinross-shire', 'Kirkcudbrightshire', 'Lanarkshire', 'Midlothian', 'Moray', 'Nairnshire', 'Orkney', 'Peeblesshire', 'Perthshire', 'Renfrewshire', 'Ross-shire', 'Roxburghshire', 'Selkirkshire', 'Shetland', 'Stirlingshire', 'Sutherland', 'West Lothian', 'Wigtownshire', 'Antrim', 'Armagh', 'Down', 'Fermanagh', 'Londonderry', 'Tyrone'];
   var countryList = ['Afghanistan', 'Albania', 'Algeria', 'Andorra', 'Angola', 'Anguilla', 'Antigua &amp; Barbuda', 'Argentina', 'Armenia', 'Aruba', 'Australia', 'Austria', 'Azerbaijan', 'Bahamas' , 'Bahrain', 'Bangladesh', 'Barbados', 'Belarus', 'Belgium', 'Belize', 'Benin', 'Bermuda', 'Bhutan', 'Bolivia', 'Bosnia &amp; Herzegovina', 'Botswana', 'Brazil', 'British Virgin Islands', 'Brunei', 'Bulgaria', 'BurkinaFaso', 'Burundi', 'Cambodia', 'Cameroon', 'Canada', 'Cape Verde', 'Cayman Islands', 'Chad', 'Chile', 'China', 'Colombia', 'Congo', 'Cook Islands', 'Costa Rica', 'Cote D\' Ivoire', 'Croatia', 'Cruise Ship', 'Cuba', 'Cyprus', 'Czech Republic', 'Denmark', 'Djibouti', 'Dominica', 'Dominican Republic', 'Ecuador', 'Egypt', 'El Salvador', 'Equatorial Guinea', 'Estonia', 'Ethiopia', 'Falkland Islands', 'Faroe Islands', 'Fiji', 'Finland', 'France', 'French Polynesia', 'French West Indies', 'Gabon', 'Gambia', 'Georgia', 'Germany', 'Ghana' ,'Gibraltar','Greece','Greenland','Grenada','Guam','Guatemala','Guernsey','Guinea','Guinea Bissau', 'Guyana', 'Haiti', 'Honduras', 'Hong Kong', 'Hungary', 'Iceland', 'India', 'Indonesia', 'Iran', 'Iraq', 'Ireland', 'Isle of Man', 'Israel', 'Italy', 'Jamaica', 'Japan', 'Jersey', 'Jordan', 'Kazakhstan', 'Kenya', 'Kuwait','Kyrgyz Republic', 'Laos', 'Latvia' ,'Lebanon', 'Lesotho', 'Liberia', 'Libya', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macau', 'Macedonia', 'Madagascar', 'Malawi', 'Malaysia', 'Maldives', 'Mali', 'Malta', 'Mauritania' , 'Mauritius', 'Mexico', 'Moldova', 'Monaco', 'Mongolia', 'Montenegro', 'Montserrat', 'Morocco', 'Mozambique', 'Namibia', 'Nepal', 'Netherlands', 'Netherlands Antilles', 'New Caledonia', 'New Zealand', 'Nicaragua', 'Niger', 'Nigeria', 'Norway', 'Oman', 'Pakistan', 'Palestine', 'Panama', 'Papua New Guinea', 'Paraguay', 'Peru', 'Philippines', 'Poland', 'Portugal', 'Puerto Rico', 'Qatar', 'Reunion', 'Romania', 'Russia', 'Rwanda', 'Saint Pierre &amp; Miquelon', 'Samoa', 'San Marino', 'Saudi Arabia', 'Senegal', 'Serbia', 'Seychelles', 'Sierra Leone', 'Singapore', 'Slovakia', 'Slovenia', 'South Africa', 'South Korea', 'Spain', 'Sri Lanka', 'St Kitts &amp; Nevis', 'St Lucia', 'St Vincent', 'St. Lucia', 'Sudan', 'Suriname', 'Swaziland', 'Sweden', 'Switzerland', 'Syria', 'Taiwan', 'Tajikistan', 'Tanzania', 'Thailand', 'Togo', 'Tonga', 'Trinidad &amp; Tobago', 'Tunisia', 'Turkey', 'Turkmenistan', 'Turks &amp; Caicos', 'Uganda', 'Ukraine', 'United Arab Emirates', 'United Kingdom', 'United States', 'United States Minor Outlying Islands', 'Uruguay', 'Uzbekistan', 'Venezuela', 'Vietnam', 'Virgin Islands (US)', 'Yemen', 'Zambia', 'Zimbabwe'];
 
+
+  // Set form submit action
   $('#addressForm').submit(function(event) {
     event.preventDefault();
 
@@ -15,6 +19,8 @@ $(document).ready(function() {
     }
   });
 
+
+  // Get address details through AJX API call
   function getAddressDetails() {
     $('#formContainer').addClass('form-section--opaque');
     var postcode  = $('#postcodeInput').val();
@@ -43,14 +49,20 @@ $(document).ready(function() {
     });
   }
 
+
+  // Display error message on API error callback
   function displayErrorMessage() {
     $('#errorMessage').fadeIn(200);
   }
 
+
+  // Hide error message on form submission
   function hideErrorMessage() {
     $('#errorMessage').fadeOut(200);
   }
 
+
+  // Generate and display address dropdown on API success callback
   function generateAddressDropdown(addressList) {
     $('#errorMessage').before(
       $('<fieldset id="addressFieldset" class="form-fieldset form-fieldset--hidden"><legend class="form-fieldset__legend">Select your address:</legend><select required id="addressInput" class="form-fieldset__select form-fieldset__select--left form-fieldset__select--wide"><option value="" disabled selected hidden>Select your address</option></select></fieldset>')
@@ -65,6 +77,8 @@ $(document).ready(function() {
     });
   }
 
+
+  // Populate address details fields on address dropdown selection
   function populateAddressFields() {
     $('#addressInput').on('change', function() {
       var addressValue      = this.value;
@@ -98,6 +112,8 @@ $(document).ready(function() {
     });
   }
 
+
+  // Toggle county field based on country field selection
   function toggleUkCounties() {
     $('#countryInput').on('change', function() {
       if(this.value === 'United Kingdom') {
@@ -110,6 +126,8 @@ $(document).ready(function() {
     });
   }
 
+
+  // Get full address from address details fields on second form submission
   function getFullAddress() {
     var addressLineOne    = $('#lineOneInput').val() ? $('#lineOneInput').val() + ', ' : '';
     var addressLineTwo    = $('#lineTwoInput').val() ? $('#lineTwoInput').val() + ', ' : '';
@@ -126,6 +144,7 @@ $(document).ready(function() {
   }
 
 
+  // Generate and display address box with selected address fields values
   function addAddressBox(address, time) {
     $('<div class="form-section__inner form-section__inner--half form-section__inner--opaque"><p>'+ address +'</p><p>'+ time +'</p></div>').appendTo('#addressBoxesContainer');
 
@@ -139,6 +158,8 @@ $(document).ready(function() {
     });
   }
 
+
+  // Clean address fields for new form submission and AJAX API call
   function cleanAddressFields() {
     $('#yearsInput').val('');
     $('#monthsInput').val('');
